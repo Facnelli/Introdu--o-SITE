12359994f23777bc96830b288eac44b31e2c9a76
Guilherme Fachinelli <Facnelli@gmail.com>
2024-07-01 02:54:24 -0300

    agora vai

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: (["react-router-dom"], ["react-slick"]),
+      external: ["react", "react-dom", "react-router-dom", "react-slick"],
     },
   },
 });
